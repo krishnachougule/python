9308f6b795cb852259f76e2bf23d4b1af9e30317
krishnachougule <38999946+krishnachougule@users.noreply.github.com>
2025-07-24 14:19:52 -0500

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,21 @@
+#Assignment1
+#Task1: Perform Basic Mathematical Operations
+
+a1 = input('Enter the first number: ')
+a2 = input('Enter the second number: ')
+a1 = float(a1)
+a2 = float(a2)
+print('')
+print('')
+print('Addition: ',a1+a2)
+print('Substraction: ',a1-a2)
+print('Multiplication: ',a1*a2)
+print('Division: ',a1/a2)
+
+
+#Task2: Create a Personalized Greeting
+
+firstName = input('Enter your first Name: ')
+lastName = input('Enter your last name: ')
+
+print(f'Hello, {firstName} {lastName}! Welcome to the Python program')
---
+++ README.md
@@ -0,0 +1,4 @@
+#Assignment 1
+Assignment1.py python file has solution for Task1: Perform Basic Mathematical Operation
+
+Task2: Create a Personalized Greeting
